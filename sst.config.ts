f773b455aebfe24b5aad8c5c239f74f562a54acc
Stanislav Modrak <stanislav.modrak@gmail.com>
2025-07-28 16:34:45 +0100

    Set up AWS account

--- sst.config.ts
+++ sst.config.ts
@@ -7,6 +7,12 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      // production vs dev accounts (based on ~/.aws/config)
+      providers: {
+        aws: {
+          profile: input.stage === "production" ? "vbdhub-production" : "vbdhub-dev"
+        }
+      }
     };
   },
   async run() {
