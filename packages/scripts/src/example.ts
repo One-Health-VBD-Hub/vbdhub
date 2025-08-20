7aded66655d8d0d34d46fd10a29ed52f771670f0
Stanislav Modrak <stanislav.modrak@gmail.com>
2025-08-20 11:55:32 +0200

    Revert "Update scripts example"
    This reverts commit b35cdb2a2fa854751a9989842c0b70e20c472cbd.

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,6 +1,4 @@
-// this file can be run with `npm run shell src/example.ts`
-
 import { Resource } from "sst";
 import { Example } from "@vbdhub/core/example";
 
-console.log(`${Example.hello()} Linked to ${Resource.App.name}.`);
+console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
