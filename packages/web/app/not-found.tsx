297b3a5f59b397641bea33959b2c660cee0aaa83
Stanislav Modrak <stanislav.modrak@gmail.com>
2025-10-08 14:41:17 +0200

    Make not found server component

--- packages/web/app/not-found.tsx
+++ packages/web/app/not-found.tsx
@@ -1,14 +1,14 @@
-'use client';
-
 import Link from 'next/link';
 import { Button } from '@carbon/react';
-import { useEffect } from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Not Found - Vector-Borne Diseases Hub',
+  description:
+    'The requested page could not be found on the Vector-Borne Diseases Hub.'
+};
 
 export default function NotFound() {
-  useEffect(() => {
-    document.title = 'Not Found - Vector-Borne Diseases Hub';
-  });
-
   return (
     <div className='flex min-h-full flex-col items-center justify-center'>
       <h1 className='mb-4 text-4xl font-semibold'>404 Not Found</h1>
