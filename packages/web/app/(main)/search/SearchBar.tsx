02b0d4648b44078d1caaab60d0f8ecc49e61675c
Stanislav Modrak <stanislav.modrak@gmail.com>
2025-12-09 12:42:39 +0000

    Refactor search state

--- packages/web/app/(main)/search/SearchBar.tsx
+++ packages/web/app/(main)/search/SearchBar.tsx
@@ -1,64 +1,61 @@
-import React, { memo, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Checkbox, Search } from '@carbon/react';
 import { useMediaQuery } from 'react-responsive';
+import {
+  useCurrentPage,
+  useExactOnly,
+  useSearchQuery
+} from '@/app/(main)/search/useSearchFilters';
 
-interface SearchBarProps {
-  handleSearch: (s?: string) => void;
-  text?: string;
-  className?: string;
-  setExactOnly: (exactOnly: boolean) => void;
-  exactOnly?: boolean;
-}
+const SearchBar = ({ className }: { className?: string }) => {
+  const [, setSearchQuery] = useSearchQuery();
+  const [, setCurrentPage] = useCurrentPage();
+  const [exactOnly, setExactOnly] = useExactOnly();
 
-const SearchBar = memo(
-  ({
-    handleSearch,
-    text,
-    className,
-    setExactOnly,
-    exactOnly = false
-  }: SearchBarProps) => {
-    const [searchTerm, setSearchTerm] = useState(text);
-    const isXsScreen = useMediaQuery({ maxWidth: 480 });
+  const [searchBoxText, setSearchBoxText] = useState<string>();
+  const isXsScreen = useMediaQuery({ maxWidth: 480 });
 
-    return (
-      <div className={`flex flex-col items-end gap-3 ${className}`}>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSearch(searchTerm?.trim());
-            setSearchTerm(searchTerm?.trim());
-          }}
-          className='flex w-full gap-2'
-        >
-          <Search
-            size='lg'
-            autoComplete='on'
-            value={searchTerm}
-            placeholder={
-              isXsScreen
-                ? 'Search'
-                : 'Search using full text search, e.g. "Anopheles 2016 Insecticide resistance"'
-            }
-            labelText='Search'
-            closeButtonLabelText='Clear search input'
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <Button type='submit' title='Search for datasets'>
-            Search
-          </Button>
-        </form>
-        <Checkbox
-          id='exact-only'
-          labelText='Exact match only'
-          disabled={!searchTerm}
-          checked={exactOnly}
-          onChange={(e) => setExactOnly(e.target.checked)}
+  const handleSearch = (text?: string) => {
+    setCurrentPage(1);
+    setSearchQuery(text ?? '');
+  };
+
+  return (
+    <div className={`flex flex-col items-end gap-3 ${className}`}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch(searchBoxText?.trim());
+        }}
+        className='flex w-full gap-2'
+      >
+        <Search
+          size='lg'
+          autoComplete='on'
+          value={searchBoxText}
+          placeholder={
+            isXsScreen
+              ? 'Search'
+              : 'Search using full text search, e.g. "Anopheles 2016 Insecticide resistance"'
+          }
+          labelText='Search'
+          closeButtonLabelText='Clear search input'
+          onChange={(e) => setSearchBoxText(e.target.value)}
         />
-      </div>
-    );
-  }
-);
+        <Button type='submit' title='Search for datasets'>
+          Search
+        </Button>
+      </form>
+      <Checkbox
+        id='exact-only'
+        labelText='Exact match only'
+        disabled={!searchBoxText}
+        checked={exactOnly}
+        onChange={(e) => setExactOnly(!exactOnly)}
+      />
+    </div>
+  );
+};
 
 SearchBar.displayName = 'SearchBar';
 export default SearchBar;
